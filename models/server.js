2af75d7a12676ba5059aab01e3658c5fa3664fe5
alogan5201 <andrewthomaslogan@gmail.com>
2020-08-27 15:48:04 -0400

    update

--- models/server.js
+++ models/server.js
@@ -29,9 +29,31 @@ app.get("/", (req, res) => {
 });
 
 require("./app/routes/turorial.routes")(app);
+//Non api requests in production
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+    // Add production middleware such as redirecting to https
+
+    // Express will serve up production assets i.e. main.js
+    app.use(express.static(__dirname + '/client/build'));
+    // If Express doesn't recognize route serve index.html
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(
+            path.resolve(__dirname, 'client', 'build', 'index.html')
+        );
+    });
+}
+const proxy = require('http-proxy-middleware')
+
+module.exports = function(app) {
+    // add other server routes to path array
+    app.use(proxy(['/api' ], { target: 'http://localhost:8080' }));
+} 
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+  console.log('listening...');
+}).on('error', err => {
+  console.log(`Error Code: ${err.code}`);
 });
